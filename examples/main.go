faf0a1576b1b84b6593504af7a18ee6acad2e4ae
yasodhap-springml <yasodha.pudi@springml.com>
2022-03-24 07:10:18 +0530

    initial code

---
+++ examples/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	log "https://github.com/yasodhap-springml/sonartest"
+)
+
+func init() {
+	log.Info("Init: Example go project")
+}
+func main() {
+	log.Info("Main: Example go project")
+}
